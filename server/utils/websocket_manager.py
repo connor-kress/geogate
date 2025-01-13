ea7faa3dd05699e1e331d8c8d1e367fc03915f61
Connor Kress <ckress@ufl.edu>
2025-01-13 16:51:31 -0500

    Improved error handling in `WebSocketManager`

--- server/utils/websocket_manager.py
+++ server/utils/websocket_manager.py
@@ -9,13 +9,27 @@ class WebSocketManager:
         # Close old connection
         if user_id in self.connections:
             old_websocket = self.connections[user_id]
-            await old_websocket.close(code=1000)  # Normal closure
+            try:
+                await old_websocket.close(code=1000)
+                # RuntimeError is raised by uvicorn during handling of
+                # starlette.websockets.WebSocketDisconnect
+                # or websockets.exceptions.ConnectionClosedError
+            except RuntimeError:
+                # print(f"Old WebSocket for user {user_id} already closed")
+                pass
         self.connections[user_id] = websocket
 
     async def disconnect(self, user_id: int):
-        if user_id in self.connections:
-            websocket = self.connections[user_id]
-            await websocket.close(code=1000)  # Normal closure
+        if user_id not in self.connections:
+            print(f"User ({user_id}) not in manager (can't disconnect)")
+            return
+        websocket = self.connections[user_id]
+        try:
+            await websocket.close(code=1000)
+        except RuntimeError:
+            # print(f"WebSocket for user {user_id} was already closed")
+            pass
+        finally:
             del self.connections[user_id]
 
     async def send_message(self, message: str, user_id: int):
